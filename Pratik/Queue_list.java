45cb9f7e973b70362639635170e4d25a02e24888
Pratik Patil <ppatil0012@gmail.com>
2025-06-25 13:04:45 +0530

    Queue List

---
+++ Pratik/Queue_list.java
@@ -0,0 +1,16 @@
+package collection_frameword;
+import java.util.LinkedList;
+import java.util.Queue;
+public class Queue_list {
+
+	public static void main(String[] args)
+	{
+		Queue q1=new LinkedList();
+		q1.add(56);
+		q1.offer(52);
+		q1.offer(23);
+		System.out.println(q1);
+
+	}
+
+}
